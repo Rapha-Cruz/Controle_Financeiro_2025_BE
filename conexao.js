de38153c3c44c7c23e9e331afb133dca1b800d19
Rapha-Cruz <raphajcruz@hotmail.com>
2025-05-31 14:41:35 -0300

    alterando conexao

--- conexao.js
+++ conexao.js
@@ -3,12 +3,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    // database_url: process.env.DB_NAME_URL,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
     ssl: {
         rejectUnauthorized: false
     }
